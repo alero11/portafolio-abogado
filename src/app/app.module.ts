befc8ca1b42102d1c7959adcebeb59c8ac122d20
Alex Rolando Toro Benitez <alex_rolando@hotmail.com>
2019-02-25 12:29:41 -0400

    creacion del proyecto y componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layouts/general/footer/footer.component';
+import { NavbarComponent } from './layouts/general/navbar/navbar.component';
+import { LoginComponent } from './layouts/cms/login/login.component';
+import { PrincipalComponent } from './layouts/general/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule
